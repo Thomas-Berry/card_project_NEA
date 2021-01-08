a2f7da64082dfa1486cedca08d977ed9454b8ff2
Thomas-Berry <74414450+Thomas-Berry@users.noreply.github.com>
2021-01-08 15:00:04 +0000

    Add files via upload

--- card game/script.js
+++ card game/script.js
@@ -1,14 +1,22 @@
-cards = new Array(10);
-for(let i = 0; i < cards.length; i++){
- cards[i] = new Array(3);
-}
-let values = 0;
-let colour = 0;
-for(let i = 0; i< 10; i++){
-    for(let index = 0; index< 3; index++){
-        values = i;
-        colour = index;
-        cards[i][index] = (colour + "" + values );
+let Create_2D_Array = function(){
+    cards = new Array(10);                          // creates original array
+    for(let i = 0; i < cards.length; i++){
+     cards[i] = new Array(3);                       // creates the second dimension of the array by making each element an array
     }
+    for(let i = 0; i< 10; i++){                     
+        for(let index = 0; index< 3; index++){      // embedded for loop to fill the array
+            cards[i][index] = (index + "" + i );
+        }
+    }
+    console.log(cards);                             // print array to console to check it works
 }
-console.log(cards);
+Create_2D_Array();                                  // call the function
+
+
+
+//Development
+//Creating 2d array
+//Created a 2d array by having each element in the array be an array.
+//Then added a string to each element which was a number indicating the value and a number indicating the colour by using an embedded for loop.
+//
+//At first i tried to store the values at integers but struggled to have two separate integers for one element so changed it to strings.
